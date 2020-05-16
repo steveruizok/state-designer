39548e1694c5ea35b619484feef0eecccad8f858
Steve <steveruizok@gmail.com>
2020-05-16 21:57:57 +0100

    Update index.js

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,13 +1,12 @@
-import React from "react";
-import { ThemeProvider, Styled } from "theme-ui";
-import theme from "./theme";
-import components from "./components";
+import React from "react"
+import { ThemeProvider, Styled } from "theme-ui"
+import theme from "./theme"
+import components from "./components"
 
 export const wrapRootElement = ({ element }) => {
-  console.log(element);
   return (
     <ThemeProvider theme={theme} components={components}>
       <Styled.root>{element}</Styled.root>
     </ThemeProvider>
-  );
-};
+  )
+}
